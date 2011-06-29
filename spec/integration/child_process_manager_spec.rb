1e8245f234b4aee80216efead88639ea90c2ba0d
Christoph Sturm <me@christophsturm.com>
2011-06-29 17:59:24 +0200

    add a small spec that doesnt test much yet require timeout.rb to fix spec

---
+++ spec/integration/child_process_manager_spec.rb
@@ -0,0 +1,15 @@
+$LOAD_PATH << File.expand_path('../../../lib', __FILE__)
+
+require "rspec"
+require "child-process-manager"
+
+describe "Child Process Manager" do
+
+  it "spawns processes" do
+    ChildProcessManager.spawn(
+        :port=> 11212,
+        :cmd=>  'memcached -p 11212 -l 127.0.0.1'
+      )
+
+  end
+end
