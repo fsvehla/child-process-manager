66e8e8c75624b297b7d89c8628611346adaa6af7
Ferdinand Svehla <f.svehla@gmail.com>
2011-09-07 14:58:21 +0200

    Break up in multiple files

---
+++ lib/child_process_manager.rb
@@ -0,0 +1,50 @@
+#require 'child-process-manager/child_process'
+
+module ChildProcessManager
+  autoload :ChildProcess, 'child_process_manager/child_process'
+
+  class << self
+    def managed_processes
+      @managed_processes ||= []
+    end
+  end
+
+  # Returns an array of spawned processes. Processes that were already spawned,
+  # aren't returned.
+  def self.add(process_options)
+    child_process = ChildProcess.new(process_options)
+
+    unless managed_processes.any? { |p| p.hash == child_process.hash }
+      managed_processes << child_process
+      child_process
+    end
+  end
+
+  def self.spawn(process_options)
+    if process = self.add(process_options)
+      process.start
+    end
+  end
+
+  def self.reap_all
+    managed_processes.each do |process|
+      process.stop
+    end
+  end
+
+  def self.reap_one(*args)
+    port = 0
+    ip   = '127.0.0.1'
+
+    if args.size == 1
+      port = args[0]
+    elsif args.size == 2
+      ip  = args[0]
+      port = args[1]
+    end
+
+    if process = managed_processes.find { |p| p.port == port && p.ip == ip }
+      process.stop
+    end
+  end
+end
